8c487ecec3dade27d186a4cc45fd5894092e177a
Isaul95 <rihr_952@hotmail.com>
2020-10-31 18:54:41 -0600

    iconos

--- application/views/layouts/aside.php
+++ application/views/layouts/aside.php
@@ -16,21 +16,28 @@
 
                     <li class="treeview">
                         <a href="#">
-                            <i class="fab fa-sellsy"></i> <span>Finanzas</span>
+                            <i class="fas fa-balance-scale"></i> <span>Finanzas</span>
                             <span class="pull-right-container">
                                 <i class="fa fa-angle-left pull-right"></i>
                             </span>
                         </a>
                         <ul class="treeview-menu">
-                            <li><a href="<?php echo base_url();?>mantenimiento/comida">
-                                <i class="fas fa-utensils"></i> Comidas</a></li>
+                          <li><a href="<?php echo base_url();?>mantenimiento/comida">
+                              Formato de registro de pago</a></li>
+                              <!-- <i class="fas fa-utensils"></i> -->
 
-                            <li><a href="<?php echo base_url();?>mantenimiento/bebidas">
-                                <i class="fas fa-wine-bottle"></i> Bebidas</a></li>
+                          <li><a href="<?php echo base_url();?>mantenimiento/bebidas">
+                              Registrar pago <br> (Formato/Archivo)</a></li>
+                              <!-- <i class="fas fa-wine-bottle"></i> -->
 
 
-                            <li><a href="<?php echo base_url();?>mantenimiento/extras">
-                                <i class="fas fa-plus-circle"></i> Extras</a></li>
+                          <li><a href="<?php echo base_url();?>mantenimiento/extras">
+                              Solicitar recibo pago <br> (Formato solicitud)</a></li>
+                              <!-- <i class="fas fa-plus-circle"></i> -->
+
+                          <li><a href="<?php echo base_url();?>mantenimiento/extras">
+                            Consulta pagos</a></li>
+                            <!--   <i class="fas fa-plus-circle"></i> -->
 
                         </ul>
                     </li>
@@ -39,7 +46,7 @@
 
                     <li class="treeview">
                         <a href="#">   <!--- class="fa fa-cogs" -->
-                            <i class="fab fa-sellsy"></i><span>Cont. academico y escolar</span>
+                            <i class="fas fa-graduation-cap"></i><span>Cont. academico y escolar</span>
                             <span class="pull-right-container">
                                 <i class="fa fa-angle-left pull-right"></i>
                             </span>

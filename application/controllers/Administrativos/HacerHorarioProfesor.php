247a57da37612110b043ee2ee9ff2b7773e24435
Alexis <alekszyzz@hotmail.com>
2021-01-30 17:21:27 -0600

    Validacion_horario_profeor

--- application/controllers/Administrativos/HacerHorarioProfesor.php
+++ application/controllers/Administrativos/HacerHorarioProfesor.php
@@ -11,6 +11,7 @@ class HacerHorarioProfesor extends CI_Controller {
 	 }
 
 
+	 
 	public function index(){
 		$data = array(
 			// 'tipoDePagos' => $this->Modelo_DarAccesoAlumnos->getTipoDePagos(),
